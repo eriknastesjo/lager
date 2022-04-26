527f71aa9d42d785f155cbb41296a195f0b74423
eriknastesjo <erna21@student.bth.se>
2022-04-26 08:56:55 +0100

    Minor change

--- components/ship/ShipOrder.tsx
+++ components/ship/ShipOrder.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { Text, View, ScrollView, StyleSheet } from "react-native";
 import { Base, Typography, Table } from "../../styles";
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import getCoordinates from '../../models/nominatim';
 import * as Location from 'expo-location';
 import { DataTable } from 'react-native-paper';
@@ -23,7 +22,9 @@ export default function ShipOrder({ route }) {
             setMarker(<Marker
                 coordinate={{ latitude: parseFloat(results[0].lat), longitude: parseFloat(results[0].lon) }}
                 title={results[0].display_name}
+                identifier="there"
             />);
+
 
             setInitRegion({
                 latitude: parseFloat(results[0].lat),
@@ -31,6 +32,14 @@ export default function ShipOrder({ route }) {
                 latitudeDelta: 0.1,
                 longitudeDelta: 0.1,
             })
+
+            // setInitRegion({
+            //     latitude: 60.128161,
+            //     longitude: 18.643501,
+            //     latitudeDelta: 20,
+            //     longitudeDelta: 20,
+            // })
+
 
         })();
     }, []);
@@ -53,10 +62,10 @@ export default function ShipOrder({ route }) {
                 }}
                 title="Min plats"
                 pinColor="blue"
+                identifier="here"
             />);
         })();
     }, []);
-
 
     // const orderItemsTable = order.order_items.map((item, index) => {
     //     return <Text key={index} style={Typography.normal}>
@@ -102,7 +111,9 @@ export default function ShipOrder({ route }) {
                     loadingEnabled={true}
                     loadingIndicatorColor='#FF9731'
                     style={styles.map}
-                    initialRegion={initRegion}>
+                    initialRegion={initRegion}
+                    // fitToSuppliedMarkers={["there", "here"]}
+                >
 
                     {marker}
                     {locationMarker}
