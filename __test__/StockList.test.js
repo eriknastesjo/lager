80cadb93141c7c97f5c4f57a3a70c45d2db59f77
eriknastesjo <erna21@student.bth.se>
2022-05-02 16:27:56 +0100

    Added test for component StockList

---
+++ __test__/StockList.test.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react-native';
+import StockList from '../components/home/StockList';
+
+const products = [
+    { name: "Shampoo", stock: 2 },
+    { name: "Balsam", stock: 3 },
+    { name: "Tvål", stock: 15 },
+];
+
+// en funktion som returnerar något godtyckligt (spelar ingen roll vad) för att
+// hindra products från att bli överskuggad (overridden)
+const setProducts = () => "false";
+
+test('List should contain three items', async () => {
+    const { getByText } = render(<StockList products={products} setProducts={setProducts} />);
+
+    // // uncomment to see what is rendered in StockList component:
+    // const { getByText, debug } = render(<StockList products={products} setProducts={setProducts} />);
+    // debug("Stocklist component");
+
+    const shampoo = await getByText('Shampoo', { exact: false });
+    const balsam = await getByText('Balsam', { exact: false });
+    const soap = await getByText('Tvål', { exact: false });
+
+    expect(shampoo).toBeDefined();
+    expect(balsam).toBeDefined();
+    expect(soap).toBeDefined();
+});
